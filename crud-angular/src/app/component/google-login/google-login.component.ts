26f130d686abf595425ab93e8bb247e085c829cd
Sithumini Kaushalya <118107125+sithuminikaushalya@users.noreply.github.com>
2024-07-21 16:51:07 +0530

    Re-added crud-angular directory without nested .git

---
+++ crud-angular/src/app/component/google-login/google-login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-google-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './google-login.component.html',
+  styleUrl: './google-login.component.css'
+})
+export class GoogleLoginComponent {
+
+}
